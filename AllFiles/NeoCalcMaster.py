3b6533d7e14589decc4b9937f6b0729cb3f50ba7
Alexey <118394879+kijiassa@users.noreply.github.com>
2022-11-22 19:45:11 +0400

    Started Files

--- AllFiles/NeoCalcMaster.py
+++ AllFiles/NeoCalcMaster.py
@@ -2,25 +2,43 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
+
 
 
 class FirstWindow(Screen):
-    pass
+    def fentBtn(self):
+        sm.current = "fentanil"
+
+    def aboutBtn(self):
+        sm.current = "about"
 
 class FentanilWindow(Screen):
-    pass
+    def back(self):
+        sm.current = "first"
 
 class AboutWindow(Screen):
-    pass
+    def back(self):
+        sm.current = "first"
 
 class WindowManager(ScreenManager):
     pass
 
 kv = Builder.load_file("NeoCalcKivy.kv")
 
+sm = WindowManager()
+
+screens = [FirstWindow(name="first"), FentanilWindow(name="fentanil"), AboutWindow(name="about")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "first"
+
+
 class NeonatalApp(App): #Neonatal - это заголовок
     def build(self):
-        return kv
+        return sm
 
 if __name__ == "__main__":
     NeonatalApp().run()
