0d5076715b41d6356ccbc9f215978a1730a60949
Alexey <118394879+kijiassa@users.noreply.github.com>
2022-11-30 02:40:19 +0400

    Correct Fentanil Windwo

--- AllFiles/NeoCalcMaster.py
+++ AllFiles/NeoCalcMaster.py
@@ -2,30 +2,26 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
-
-
+#Сначала настройка окон, какое есть какое и их обозначение для дальнейшего переключения
 class FirstWindow(Screen):
     def fentBtn(self):
         sm.current = "fentanil"
-
     def aboutBtn(self):
         sm.current = "about"
-
 class FentanilWindow(Screen):
     def back(self):
         sm.current = "first"
-
 class AboutWindow(Screen):
     def back(self):
         sm.current = "first"
-
 class WindowManager(ScreenManager):
     pass
-
+#Установка и подключение киви файла
 kv = Builder.load_file("NeoCalcKivy.kv")
 
 sm = WindowManager()
